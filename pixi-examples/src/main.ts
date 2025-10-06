d836da987619caf5eb1097d0bd99a782aa84e751
boris.blagojevic <boris.blagojevicc@hotmaail.com>
2025-10-06 14:02:57 +0200

    Added dynamic

--- pixi-examples/src/main.ts
+++ pixi-examples/src/main.ts
@@ -53,13 +53,12 @@ let state, dungeon, door, blobs, explorer: PIXI.Sprite, treasure, gameScene, gam
     treasure.y = gameScene.height / 2 - treasure.height / 2;
     gameScene.addChild(...[explorer, treasure]);
 
-    blobs = new Blobs(6, spritesheet.textures['blob.png'], app.stage);
-    gameScene.addChild(...blob.getBlobs());
-
+    const blobGroup = new Blobs(6, spritesheet.textures['blob.png'], app.stage);
+    gameScene.addChild(...blobGroup.getBlobs());
+    blobs = blobGroup.getBlobs();
 
     healtBar = new HealthBar(app.stage);
     gameScene.addChild(healtBar.healthBar);
-    healtBar.setHealbar();
 
     const style = new PIXI.TextStyle({
         fontFamily: "Futura",
@@ -75,20 +74,141 @@ let state, dungeon, door, blobs, explorer: PIXI.Sprite, treasure, gameScene, gam
     gameScene.visible = true;
     gameOverScene.visible = false;
 
+    const left = keyboard(37),
+        up = keyboard(38),
+        right = keyboard(39),
+        down = keyboard(40);
 
-    // app.ticker.add((delta) => gameLoop(delta));
+    left.press = function() {
+
+        explorer.vx = -5;
+        explorer.vy = 0;
+    };
+
+    left.release = function() {
+
+        if (!right.isDown && explorer.vy === 0) {
+            explorer.vx = 0;
+        }
+    };
+
+    up.press = function() {
+        explorer.vy = -5;
+        explorer.vx = 0;
+    };
+    up.release = function() {
+        if (!down.isDown && explorer.vx === 0) {
+            explorer.vy = 0;
+        }
+    };
+
+    right.press = function() {
+        explorer.vx = 5;
+        explorer.vy = 0;
+    };
+    right.release = function() {
+        if (!left.isDown && explorer.vy === 0) {
+            explorer.vx = 0;
+        }
+    };
+
+    down.press = function() {
+        explorer.vy = 5;
+        explorer.vx = 0;
+    };
+    down.release = function() {
+        if (!up.isDown && explorer.vx === 0) {
+            explorer.vy = 0;
+        }
+    };
+
+    state = play;
+
+    app.ticker.add((delta) => gameLoop(delta));
 
 })();
 
-// function gameLoop(delta) {
-//     //Runs the current game `state` in a loop and renders the sprites
-// }
-//
+function gameLoop(delta) {
+    state(delta);
+}
+
 function play(delta) {
+
+
+    //use the explorer's velocity to make it move
     explorer.x += explorer.vx;
     explorer.y += explorer.vy;
 
+    //Contain the explorer inside the area of the dungeon
     contain(explorer, {x: 28, y: 10, width: 488, height: 480});
+    //contain(explorer, stage);
+
+    //Set `explorerHit` to `false` before checking for a collision
+    let explorerHit = false;
+
+    //Loop through all the sprites in the `enemies` array
+    blobs.forEach(function(blob) {
+
+        //Move the blob
+        blob.y += blob.vy;
+
+        //Check the blob's screen boundaries
+        const blobHitsWall = contain(blob, {x: 28, y: 10, width: 488, height: 480});
+
+        //If the blob hits the top or bottom of the stage, reverse
+        //its direction
+        if (blobHitsWall === "top" || blobHitsWall === "bottom") {
+            blob.vy *= -1;
+        }
+
+        //Test for a collision. If any of the enemies are touching
+        //the explorer, set `explorerHit` to `true`
+        if (hitTestRectangle(explorer, blob)) {
+            explorerHit = true;
+        }
+    });
+
+    //If the explorer is hit...
+    if (explorerHit) {
+
+        //Make the explorer semi-transparent
+        explorer.alpha = 0.5;
+
+        //Reduce the width of the health bar's inner rectangle by 1 pixel
+        healtBar.healthBar.outer.width -= 1;
+
+    } else {
+
+        //Make the explorer fully opaque (non-transparent) if it hasn't been hit
+        explorer.alpha = 1;
+    }
+
+    //Check for a collision between the explorer and the treasure
+    if (hitTestRectangle(explorer, treasure)) {
+
+        //If the treasure is touching the explorer, center it over the explorer
+        treasure.x = explorer.x + 8;
+        treasure.y = explorer.y + 8;
+    }
+
+    //Does the explorer have enough health? If the width of the `innerBar`
+    //is less than zero, end the game and display "You lost!"
+    if (healtBar.healthBar.outer.width < 0) {
+        state = end;
+        message.text = "You lost!";
+    }
+
+    //If the explorer has brought the treasure to the exit,
+    //end the game and display "You won!"
+    if (hitTestRectangle(treasure, door)) {
+        state = end;
+        message.text = "You won!";
+    }
+}
+
+function end() {
+    gameScene.visible = false;
+    gameOverScene.visible = true;
 }
 
 function contain(sprite: PIXI.Sprite, container: { x: any; y: any; width: any; height: any; }) {
@@ -123,26 +243,90 @@ function contain(sprite: PIXI.Sprite, container: { x: any; y: any; width: any; h
     return collision;
 }
 
-// @ts-ignore
-blobs.forEach(function(blob) {
+function hitTestRectangle(r1, r2) {
 
-    //Move the blob
-    blob.y += blob.vy;
+    //Define the variables we'll need to calculate
+    let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
 
-    //Check the blob's screen boundaries
-    const blobHitsWall = contain(blob, {x: 28, y: 10, width: 488, height: 480});
+    //hit will determine whether there's a collision
+    hit = false;
 
-    if (blobHitsWall === "top" || blobHitsWall === "bottom") {
-        blob.vy *= -1;
+    //Find the center points of each sprite
+    r1.centerX = r1.x + r1.width / 2;
+    r1.centerY = r1.y + r1.height / 2;
+    r2.centerX = r2.x + r2.width / 2;
+    r2.centerY = r2.y + r2.height / 2;
+
+    //Find the half-widths and half-heights of each sprite
+    r1.halfWidth = r1.width / 2;
+    r1.halfHeight = r1.height / 2;
+    r2.halfWidth = r2.width / 2;
+    r2.halfHeight = r2.height / 2;
+
+    //Calculate the distance vector between the sprites
+    vx = r1.centerX - r2.centerX;
+    vy = r1.centerY - r2.centerY;
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+    combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+        //A collision might be occurring. Check for a collision on the y axis
+        if (Math.abs(vy) < combinedHalfHeights) {
+
+            //There's definitely a collision happening
+            hit = true;
+        } else {
+
+            //There's no collision on the y axis
+            hit = false;
+        }
+    } else {
+
+        //There's no collision on the x axis
+        hit = false;
     }
 
-    // if (hitTestRectangle(explorer, blob)) {
-    //     explorerHit = true;
-    // }
-});
+    //`hit` will be either `true` or `false`
+    return hit;
+};
 
+function keyboard(keyCode:number) {
+    const key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
 
-//
-// function end() {
-//     //All the code that should run at the end of the game
-// }
+    key.downHandler = function(event:Event) {
+        if (event.keyCode === key.code) {
+            if (key.isUp && key.press) {
+                key.press();
+            }
+            key.isDown = true;
+            key.isUp = false;
+        }
+        event.preventDefault();
+    };
+
+    //The `upHandler`
+    key.upHandler = function(event: Event) {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) {
+                key.release();
+            }
+            key.isDown = false;
+            key.isUp = true;
+        }
+        event.preventDefault();
+    };
+
+    //Attach event listeners
+    window.addEventListener("keydown", key.downHandler.bind(key), false);
+    window.addEventListener("keyup", key.upHandler.bind(key), false);
+    return key;
+}
